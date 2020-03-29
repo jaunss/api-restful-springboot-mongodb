873e6dc0f4f78d01ea05d111559f50968020395e
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-03-29 15:27:27 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/joaog/apimongodb/resources/UserResource.java
+++ src/main/java/com/joaog/apimongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaog.apimongodb.domain.Post;
 import com.joaog.apimongodb.domain.User;
 import com.joaog.apimongodb.dto.UserDTO;
 import com.joaog.apimongodb.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
